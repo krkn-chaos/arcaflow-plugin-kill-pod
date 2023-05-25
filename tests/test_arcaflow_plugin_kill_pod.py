86e75b85af7284188ad734fc1952bb79fac96f13
Paige Rubendall <prubenda@redhat.com>
2023-05-25 11:51:14 -0400

    more indenting

--- tests/test_arcaflow_plugin_kill_pod.py
+++ tests/test_arcaflow_plugin_kill_pod.py
@@ -72,14 +72,14 @@ class KillPodTest(unittest.TestCase):
                 )
             )
             pods = arcaflow_plugin_kill_pod._find_pods(core_v1, None,
-                            namespace_pattern=re.compile("^default$"),
-                            name_pattern=re.compile(
+                                namespace_pattern=re.compile("^default$"),
+                                name_pattern=re.compile(
                                 "^" + re.escape(pod.metadata.name) + "$"
                             ))
             print("pods " + str(pods))
             while len(pods) == 0:
                 pods = arcaflow_plugin_kill_pod._find_pods(core_v1, None, namespace_pattern=re.compile("^default$"),
-                    name_pattern=re.compile(
+                            name_pattern=re.compile(
                             "^" + re.escape(pod.metadata.name) + "$"
                     ))
                 print("pods " + str(pods))
